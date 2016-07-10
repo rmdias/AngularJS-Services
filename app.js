fde2aa15ce09aeb253bc46300b6434d2bb1ab6aa
Rodolfo Dias <rmdias29@gmail.com>
2016-07-10 14:00:00 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 (function(){
   'use strict';
   angular
-  .module('heroesApp', [
-    'heroesApp.heroesProvider',
-    'heroesApp.heroesService',
-    'heroesApp.heroesFactory',
-    'heroesApp.heroesController',
-    'heroesApp.heroesConstant',
-    'heroesApp.heroesValue'
-  ])
+    .module('heroesApp', [
+      'heroesApp.heroesProvider',
+      'heroesApp.heroesService',
+      'heroesApp.heroesFactory',
+      'heroesApp.heroesController',
+      'heroesApp.heroesConstant',
+      'heroesApp.heroesValue'
+    ])
   .config(function(heroesProviderProvider){
     var newHeroes = ['Wesley Safadão', 'Batman', 'Captain America'];
     heroesProviderProvider.updateHeroes(newHeroes);
